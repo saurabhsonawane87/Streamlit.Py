7c0eae5bfba4808351016a508f7decac2673b35c
saurabhsonawane87 <135195526+saurabhsonawane87@users.noreply.github.com>
2025-06-30 21:19:40 +0530

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -9,7 +9,7 @@ np.random.seed(42)
 df = pd.DataFrame({
     'Date': pd.date_range('2024-01-01', periods=200),
     'Sales': np.random.randint(500, 5000, size=200),
-    'Region': np.random.choice(['Delhi', 'Mumbai', 'Pune', 'Nashil'], size=200),
+    'Region': np.random.choice(['Delhi', 'Mumbai', 'Pune', 'Nashik'], size=200),
     'Product': np.random.choice(['Electronics', 'Clothing', 'Food & Beverages'], size=200)
 })
 
@@ -31,7 +31,7 @@ with col1:
     st.plotly_chart(fig_line,use_container_width=True)
 with col2:
     region_sales=filtered_df.groupby('Region')['Sales'].sum().reset_index()
-    fig_bar=px.bar(region_sales,x='Region',y='Sales',title= 'Toatl Sales By Region')
+    fig_bar=px.bar(region_sales,x='Region',y='Sales',title= 'Total Sales By Region')
     st.plotly_chart(fig_bar,use_container_width=True)
 
 st.subheader("Filtered Data")
